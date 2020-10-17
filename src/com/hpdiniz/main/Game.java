a6e47e9b1aeddcc8c6d91cfad3c01c2040dac202
Henrique Penna <bcgaiuehfh@gmail.com>
2020-10-17 18:31:08 -0300

    'spritesheet'

--- src/com/hpdiniz/main/Game.java
+++ src/com/hpdiniz/main/Game.java
@@ -4,28 +4,46 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
+import com.hpdiniz.entities.Entity;
+import com.hpdiniz.entities.Player;
+import com.hpdiniz.graphics.Spritesheet;
+
+public class Game extends Canvas implements Runnable,KeyListener{
 
 	private static final long serialVersionUID = 1L;
 	public static JFrame frame;
 	private Thread thread;
 	private boolean isRunning = false;
-	private final int width = 160;
-	private final int heigth = 120;
+	private final int width = 240;
+	private final int height = 160;
 	private final int scale = 3;
 	
 	private BufferedImage image;
 	
+	public List<Entity> entities;
+	public Spritesheet spritesheet;
+	
+	public static Player player;
+	
 	public Game() {
-		setPreferredSize(new Dimension(width*scale, heigth*scale));
+		addKeyListener(this);
+		setPreferredSize(new Dimension(width*scale, height*scale));
 		initFrame();
-		image = new BufferedImage(width,heigth,BufferedImage.TYPE_INT_RGB);
+		// Inicializando objetos
+		image = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+		entities = new ArrayList<Entity>();
+		spritesheet = new Spritesheet("/spritesheet.png");
+		player = new Player(0,0,16,16,spritesheet.getSprite(32, 0,16,16));
+		entities.add(player);
 	}
 	
 	public void initFrame() {
@@ -59,7 +77,10 @@ public class Game extends Canvas implements Runnable{
 	}
 	
 	public void tick() {
-		
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.tick();
+		}
 	}
 	
 	public void render() {
@@ -69,15 +90,19 @@ public class Game extends Canvas implements Runnable{
 			return;
 		}
 		Graphics g = image.getGraphics();
-		g.setColor(new Color(255,255,255));
-		g.fillRect(0, 0, width, heigth);
+		g.setColor(new Color(0,0,0));
+		g.fillRect(0, 0, width, height);
 		
 		/* Rendereização do jogo */
 		// Graphics2D g2 = (Graphics2D) g;
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.render(g);
+		}
 		
 		g.dispose();
 		g = bs.getDrawGraphics();
-		g.drawImage(image, 0, 0, width*scale, heigth*scale, null);
+		g.drawImage(image, 0, 0, width*scale, height*scale, null);
 		bs.show();
 		
 	}
@@ -109,6 +134,44 @@ public class Game extends Canvas implements Runnable{
 		}
 		
 		stop();
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+			player.right = true;
+		} else if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+			player.left = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+			player.up = true;
+		} else if(e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+			player.down = true;
+		} 
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+			player.right = false;
+		} else if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+			player.left = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+			player.up = false;
+		} else if(e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+			player.down = false;
+		} 
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
